91c2e2e4f6519fa71983ceeb29ee5f18d48b23eb
Mikhail Titov <mikhail.a.titov@gmail.com>
2020-12-17 14:57:57 -0500

    updated processed tasks handling

--- src/radical/dreamer/managers/resource.py
+++ src/radical/dreamer/managers/resource.py
@@ -1,8 +1,12 @@
 
+import math
+
 from ..units import MultiResource, Resource, Task, Workload
 
 from ._base import Manager
 from .ext import Schedule
+
+PROFILE_MSG_SIZE = 1000
 
 
 class ResourceManager(Manager):
@@ -51,12 +55,27 @@ class ResourceManager(Manager):
 
                 self._schedule.set_tasks(self._workload.tasks_list)
 
+                # collect tasks to send back to the session
+                processed_tasks = []
+
                 while True:
 
                     idle_cores = self._resource.next_idle_cores
 
                     if not self._schedule.is_active:
                         if not self._resource.is_busy:
+                            num_msgs = math.ceil(len(processed_tasks) /
+                                                 PROFILE_MSG_SIZE)
+                            for idx in range(num_msgs):
+                                # publish profile data
+                                self._rmq.publish(
+                                    queue=self._rmq_queues.profile,
+                                    data=Task.demunch(
+                                        processed_tasks[
+                                            idx * PROFILE_MSG_SIZE:
+                                            (idx + 1) * PROFILE_MSG_SIZE]))
+                            del processed_tasks[:]
+
                             # exit processing (no new tasks, no busy cores)
                             break
                         else:
@@ -65,11 +84,7 @@ class ResourceManager(Manager):
 
                     tasks = self._schedule.get_scheduled_tasks(idle_cores)
                     self._resource.process(tasks)
-
-                    # publish profile data back to the session [manager]
-                    self._rmq.publish(
-                        queue=self._rmq_queues.profile,
-                        data=Task.demunch(tasks))
+                    processed_tasks.extend(tasks)
 
                 self._workload = None
                 self._logger.info('Scheduled tasks executed')
